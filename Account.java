300b28edc6570d97f6f3593e2368810ec35094af
CotiugaIlie <61407980+CotiugaIlie@users.noreply.github.com>
2020-03-09 09:15:52 +0200

    Add files via upload
    Class files

--- Account.java
+++ Account.java
@@ -1,24 +1,9 @@
-// Decompiled by DJ v3.12.12.101 Copyright 2016 Atanas Neshkov  Date: 3/9/2020 9:08:00 AM
-// Home Page:  http://www.neshkov.com/dj.html - Check often for new version!
-// Decompiler options: packimports(3) 
-// Source File Name:   Account.java
-
-import java.io.PrintStream;
-
-public class Account
-{
-
-    public Account()
-    {
-    }
-
-    void Login(String username, String password)
-    {
-        System.out.printf("Logged in as : %s .", new Object[] {
-            username
-        });
-    }
-
+public class Account {
     public String username;
     public String password;
+
+    void Login(String username,String password){
+        System.out.printf("Logged in as : %s .",username);
+
+    }
 }
--- Persoana.java
+++ Persoana.java
@@ -1,31 +1,30 @@
-// Decompiled by DJ v3.12.12.101 Copyright 2016 Atanas Neshkov  Date: 3/9/2020 9:09:55 AM
-// Home Page:  http://www.neshkov.com/dj.html - Check often for new version!
-// Source File Name:   Persoana.java
 public class Persoana {
-    private ShoppingCart sh = new ShoppingCart();
+    private ShoppingCart sh;
     private String name;
 
     Persoana(String n) {
-        this.name = n;
+        sh = new ShoppingCart();
+        name = n;
     }
 
-    public void add(Item it) {
-        this.sh.add(it);
+    public void add(Item it){
+        sh.add(it);
     }
 
     public void delete(Item it) {
-        this.sh.delete(it);
+        sh.delete(it);
     }
 
     public int getTotal() {
-        return this.sh.getTotal();
+        return sh.getTotal();
     }
 
     public void checkOut() {
-        this.sh.checkOut();
+        sh.checkOut();
     }
 
     public void show() {
-        throw new IllegalStateException("Decompilation failed");
+        System.out.print("Buna ziua, " + name + "! ");
+        sh.show();
     }
 }
